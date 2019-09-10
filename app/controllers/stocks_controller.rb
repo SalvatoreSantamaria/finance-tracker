5027f419930368835b4faf17422c3c28cf06110f
Salvatore Santamaria <sam.santamaria@gmail.com>
2019-09-10 08:17:27 -0700

    Now validating stock searches

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,8 +1,18 @@
 class StocksController < ApplicationController
     def search
-        @stock = Stock.new_from_lookup(params[:stock]) #storing in stock object, stock class is returning object back
-        #render json: @stock #this will render in json on page! 
-        render 'users/my_portfolio'
+        if params[:stock].present? #if stock exists
+            @stock = Stock.new_from_lookup(params[:stock]) #storing in stock object, stock class is returning object back
+            #render json: @stock #this will render in json on page! 
+        if @stock    
+            render 'users/my_portfolio'
+        else
+            flash[:danger] = "You have entered an incorrect ticker symbol"
+            redirect_to my_portfolio_path
+        end       
+        else 
+            flash[:danger] = "Cannot enter an empty search string"
+            redirect_to my_portfolio_path
+        end
     end
 # from text directions
 #   def search
