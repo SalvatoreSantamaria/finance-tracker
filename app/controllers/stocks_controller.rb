9b9864357f031f34efc984a016cf0d328fb29a86
Salvatore Santamaria <sam.santamaria@gmail.com>
2019-09-10 08:55:38 -0700

    Added Ajax call for looking up stock quote

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,8 +3,10 @@ class StocksController < ApplicationController
         if params[:stock].present? #if stock exists
             @stock = Stock.new_from_lookup(params[:stock]) #storing in stock object, stock class is returning object back
             #render json: @stock #this will render in json on page! 
-        if @stock    
-            render 'users/my_portfolio'
+            if @stock    
+                respond_to do |format| #responds to js format - this is for using ajax
+                format.js {render partial: 'users/result'}
+            end
         else
             flash[:danger] = "You have entered an incorrect ticker symbol"
             redirect_to my_portfolio_path
@@ -19,5 +21,4 @@ class StocksController < ApplicationController
 #     @stock = Stock.new_from_lookup(params[:stock])
 #     render 'users/my_portfolio'
 #   end
-
 end
