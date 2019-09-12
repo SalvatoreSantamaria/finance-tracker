33ba6df130bdde6e3c32e34fb1b00ac7af8dcc5b
Salvatore Santamaria <sam.santamaria@gmail.com>
2019-09-12 08:40:20 -0700

    Added stock restrictions

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,7 +2,7 @@ class Stock < ApplicationRecord #stock model
   has_many :user_stocks
   has_many :users, through: :user_stocks
   
-  def self.find_by_ticker(ticker_symbol)
+  def self.find_by_ticker(ticker_symbol) #look up stock in the db from the stocks table
     where(ticker: ticker_symbol).first
   end
  
