9ddfe701d719d06369b2d419b30a903ab20a362d
Salvatore Santamaria <sam.santamaria@gmail.com>
2019-09-25 08:14:23 -0700

    Can add and remove friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,4 +22,18 @@ class UsersController < ApplicationController
             format.js { render partial: 'friends/result' }
             end
         end
+
+    def add_friend
+        @friend = User.find(params[:friend])
+        current_user.friendships.build(friend_id: @friend.id) #building an association
+
+        if current_user.save
+            flash[:success] = "Friend was successfully added"
+            redirect_to my_friends_path
+        else 
+            flash[:danger] = "There was something wrong with the friend request"
+            redirect_to my_friends_path
+        end
+    end
+        
     end
